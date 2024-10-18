aad09f35b8a30221aaf8b06581c963405a2a4e92
Evelyn Tengler <58312881+etengler@users.noreply.github.com>
2024-10-18 14:03:21 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 #SOLAPA IZQUIERDA
-logo_ign = "https://github.com/etengler/Aplicaciones-DIG_v0.0/blob/c0b669c3dc073bf9289ba50ac1ad5dd419d7c43b/recursos/logo_ign_largo_azul.png"
+logo_ign = 'recursos/logo_ign_largo_azul.png'
 st.logo(logo_ign)
 
 
